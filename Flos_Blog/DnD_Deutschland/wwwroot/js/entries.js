301007646b95b0af0c1c4a877a8e489f020a3bf5
Florian Delvo <fdelvo@students.uni-mainz.de>
2017-07-13 08:13:50 +0200

    entries

--- Flos_Blog/DnD_Deutschland/wwwroot/js/entries.js
+++ Flos_Blog/DnD_Deutschland/wwwroot/js/entries.js
@@ -1,22 +1,20 @@
-﻿const getEntry = function (e) {
-    console.log($(e).attr("id"));
-    /*$.ajax({
+﻿$(document).ready(function () {
+    $("article[id^='entry-']").each(function () {
+        var keyword = this.id.substring(6);
+        $.ajax({
             type: "GET",
-            url: `/api/entries/getentrybytitle/${title}`,
+            url: `/api/entries/getentrybytitle/${keyword}`,
             contentType: "application/json; charset=utf-8",
             dataType: "json"
         })
-        .done(function (data) {
-            $(`#entry-${title}`).val(data.entryTitle);
-        })
-        .fail(function () {
+            .done(function (data) {
+                $(`#entry-${keyword}`).append(`<h2>${data.entryTitle}</h2><p>${data.entryContent}</p>`);
+            })
+            .fail(function () {
 
-        })
-        .always(function () {
+            })
+            .always(function () {
 
-        });*/
-};
-
-$(document).ready(function () {
-    $("article[id^='entry-']").each(getEntry());
+            });
+    });
 });
